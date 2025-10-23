3efb04056dd2f5716a3fe379f7242d485ad88bef
Umesh-codes <umesha.bm@makelooplabs.com>
2025-10-23 19:49:48 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -6,29 +6,36 @@ const BASE_URL = process?.env?.VITE_BASE_URL ?? "";
 
 /** @type {import("vite").UserConfig} */
 export default defineConfig({
-	base: BASE_URL,
-	build: {
-		minify: true,
-		cssMinify: true,
-		target: "esnext",
-		// cssCodeSplit: false,
-		// modulePreload: false,
-		assetsDir: "assets",
-		chunkSizeWarningLimit: 1000,
-	},
-	server: {
-		host: "0.0.0.0",
-	},
-	plugins: [
-		react(),
-		federation({
-			name: "mf_remote_brik",
-			filename: "remoteEntry.js",
-			shared: ["react", "react-dom", "@koadz/core", "@koadz/brik-wrapper", "@koadz/playground"],
-			exposes: {
-				// Expose files to other federated modules
-				// "example_name": "./path/to/file.jsx", // can be js or jsx
-			},
-		}),
-	],
+  base: BASE_URL,
+  build: {
+    minify: true,
+    cssMinify: true,
+    target: "esnext",
+    // cssCodeSplit: false,
+    // modulePreload: false,
+    assetsDir: "assets",
+    chunkSizeWarningLimit: 1000,
+  },
+  server: {
+    host: "0.0.0.0",
+  },
+  plugins: [
+    react(),
+    federation({
+      name: "mf_remote_brik",
+      filename: "remoteEntry.js",
+      shared: [
+        "react",
+        "react-dom",
+        "@koadz/core",
+        "@koadz/brik-wrapper",
+        "@koadz/playground",
+      ],
+      exposes: {
+        // Expose files to other federated modules
+        // "example_name": "./path/to/file.jsx", // can be js or jsx
+        "./GroupImageCards": "./src/components/Brik.jsx",
+      },
+    }),
+  ],
 });
